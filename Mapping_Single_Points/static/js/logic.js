81969e80ae9d88f8c5b35f35f4a4c5404afa0dbf
tabithacharlotte <tabithacharlotte24@gmail.com>
2020-10-11 13:13:39 -0700

    mapping a single point in LA and then changing tile color to black and grey

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,13 +4,26 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
+// // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
+// Alternate tile layer
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',     maxZoom: 18,
+accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+//  Add a marker to the map for Los Angeles, California.
+// creates a marker looking like the google maps pin: let marker = L.marker([34.0522, -118.2437]).addTo(map);
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
